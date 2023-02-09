7714e2745102eeb6e2afcf5cb730375ce0b304a3
Jaap de Jong <jaapdejong15@gmail.com>
2023-02-09 14:58:51 +0100

    Add converting of equivalence classes to equalities

--- src/specification_extractor.jl
+++ src/specification_extractor.jl
@@ -10,7 +10,9 @@ function extract_specifications(
         max_depth::Int, 
         start_symbol::Symbol, 
         data_generators, 
-        batch_size::Int=64)
+        batch_size::Int=64,
+        num_batches_after_last_split::Int=5
+        )
     # Identify the input variables in the grammar
     input_variables::Vector{NamedTuple{(:var, :type), Tuple{Symbol, Symbol}}} = []
     for (i, rule) ∈ enumerate(grammar.rules)
@@ -24,10 +26,11 @@ function extract_specifications(
 
     equivalence_classesᵢ = [programs]
     equivalence_classesᵢ₊₁ = []
+    batches_since_last_split = 0
 
     symboltable::SymbolTable = SymbolTable(grammar)
 
-    for _ ∈ 1:10
+    while true
         for equivalence_class ∈ equivalence_classesᵢ
             tests = []
             # create tests
@@ -47,8 +50,45 @@ function extract_specifications(
             end
             append!(equivalence_classesᵢ₊₁, values(new_classes))
         end
+        # TODO: Keep track of splits for batches individually
+        if length(equivalence_classesᵢ) == length(equivalence_classesᵢ₊₁)
+            batches_since_last_split += 1
+        else 
+            batches_since_last_split = 0
+        end
+
         equivalence_classesᵢ = equivalence_classesᵢ₊₁
         equivalence_classesᵢ₊₁ = []
+        if batches_since_last_split ≥ num_batches_after_last_split
+            return equivalence_classesᵢ
+        end
     end
-    return equivalence_classesᵢ
+end
+
+_expr_depth_and_size(::Symbol) = (0, 1)
+_expr_depth_and_size(::Any) = (0, 1)
+
+function _expr_depth_and_size(e::Expr)::Tuple{Int, Int}
+    if length(e.args) == 0
+        return (0, 1)
+    end
+    child_depth_size = collect(zip(collect(map(_expr_depth_and_size, e.args))...))
+    return (maximum(child_depth_size[1], init=0) + 1, sum(child_depth_size[2]) + 1)
+end
+
+"""
+Rewrites an equivalence class to a set equalities.
+The simplest expression in the equivalence class is used in every equality.
+"""
+function equivalence2specs(equivalence_class)
+    # Find program with smallest size for the minimal depth.
+    simplest_expr = argmin(_expr_depth_and_size, equivalence_class)
+
+    equivalences::Vector{Tuple{Any, Any}} = []
+    for expr ∈ equivalence_class
+        if expr ≠ simplest_expr
+            push!(equivalences, (expr, simplest_expr))
+        end
+    end
+    return equivalences
 end
